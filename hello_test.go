fb34028bf371d2d4822ef81bec17e4c5c3180bad
Pedro Fellipe <pedrofellipeunai@gmail.com>
2022-04-30 11:23:41 -0300

    test for empty name

--- hello_test.go
+++ hello_test.go
@@ -2,11 +2,23 @@ package main
 
 import "testing"
 
-func TestOla(t *testing.T) {
-	result := Hello("Joao")
-	expected := "Olá, Joao"
+func TestOla(t *testing.T) {	
+	t.Run("should say hello to people", func(t *testing.T) {
+		result := Hello("Joao")
+		expected := "Hello, Joao"
+	
+		if result != expected{
+			t.Errorf("result '%s', expected '%s'", result, expected)
+		}
+	})
 
-	if result != expected{
-		t.Errorf("result '%s', expected '%s'", result, expected)
-	}
+	t.Run("should say 'Hello, word' when name is empty", func(t *testing.T) {
+		result := Hello("")
+		expected := "Hello, word"
+	
+		if result != expected{
+			t.Errorf("result '%s', expected '%s'", result, expected)
+		}
+	})
+	
 }
